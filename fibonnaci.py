5066c9e3f41588dcd5282328846bcb62cc124a46
sasiashwin <41472189+sasiashwin@users.noreply.github.com>
2021-05-31 07:40:14 +0530

    Update fibonnaci.py

--- fibonnaci.py
+++ fibonnaci.py
@@ -11,14 +11,13 @@ def fibonnaci(n):
         cnt +=1 
 fibonnaci(5)
 
-
-def fibonacci(n):
+def fibonnaci(n):
     if(n==0):
         return 0
-    elif(n==1):
-        return 1
+    if(n==1):
+        return 1 
     else:
-        return fibonacci(n-2)+fibonacci(n-1)
+        return fibonnaci(n-2)+fibonnaci(n-1)
 n=5
-for i in range(0,n):
-    print(fibonacci(i),end=" ")
+for i in range(1,n+1):
+    print(fibonnaci(i),end=" ")
